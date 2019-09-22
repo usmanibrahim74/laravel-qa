3b13a7ab5e40e5b60a7ffcc6bbf9e47c915d6211
Usman Ibraheem <usmanibrahim74@gmail.com>
2019-09-22 15:39:45 +0500

    Implement form validation and store the question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -20,19 +20,21 @@
                     @csrf
                     <div class="form-group">
                         <label for="question-title">Question Title</label>
-                        <input type="text" class="form-control {{ $errors->has('title')?'is-invalid':'' }}" name="title" id="question-title">
-                        @if($errors->has('title')){
+                    <input type="text" value="{{ old('title') }}" class="form-control {{ $errors->has('title')?'is-invalid':'' }}" name="title" id="question-title">
+                        @if($errors->has('title'))
                             <div class="invalid-feedback">
-                                <strong>{{ $errors->first('title') }}</strong>
+                                <strong> {{ $errors->first('title') }}</strong>
                             </div>
                         @endif
                     </div>
                     <div class="form-group">
                         <label for="question-body">Explain your question</label>
-                        <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body')?'is-invalid':'' }}"></textarea>
-                        @if($errors->has('body')){
+                        <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body')?'is-invalid':'' }}">
+                            {{ old('body') }}
+                        </textarea>
+                        @if($errors->has('body'))
                             <div class="invalid-feedback">
-                                <strong>{{ $errors->first('body') }}</strong>
+                                <strong> {{ $errors->first('body') }}</strong>
                             </div>
                         @endif
                     </div>
